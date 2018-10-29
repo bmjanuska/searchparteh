fcf4c487ea300cda33304690ae83705d16aeeaff
Bernadette Januska <bmjanuska@gmail.com>
2018-10-28 22:08:17 -0400

    added search back to menu. logout from nav breaks app

--- src/components/Bars/TopNavbar.js
+++ src/components/Bars/TopNavbar.js
@@ -1,90 +1,3 @@
-// import React from "react";
-// import "./Navbar.css";
-// import { Link } from "react-router-dom";
-// import { Container, Row, Col } from 'reactstrap';
-
-// // Navigation 
-// // ===========================================================================
-// export const TopNavbar = props => (
-//   <Navbar className="bg-purp" expand="lg">
-//     <Navbar.Brand href="/">
-//       <img src="../images/icon-01.svg" alt="Search Party" style={{ width: 40 }} />
-//       Search Party
-//   </Navbar.Brand>
-//     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//     <Navbar.Collapse id="basic-navbar-nav">
-//       <Nav className="mr-auto">
-
-//         <Link
-          
-//             to="/"
-//             className={
-//               window.location.pathname === "/home" || window.location.pathname === "/home"
-//                 ? "nav-link active"
-//                 : "nav-link"
-//             }
-//           >
-//             Home
-        
-//         </Link>
-
-//         <Link
-        
-//             to="/challenge"
-//             className={
-//               window.location.pathname === "/challenge" || window.location.pathname === "/challenge"
-//                 ? "nav-link active"
-//                 : "nav-link"
-//             }
-//           >
-//             Challenge
-        
-//         </Link>
-
-//         <Link
-//             to="/user"
-//             className={
-//               window.location.pathname === "/user"
-//                 ? "nav-link active"
-//                 : "nav-link"
-//             }
-//           >
-//             My Account
-          
-//         </Link>
-
-//         <Link
-//             to="/search"
-//             className={
-//               window.location.pathname === "/search"
-//                 ? "nav-link active"
-//                 : "nav-link"
-//             }
-//           >
-//             Search
-          
-//         </Link>
-
-//         <Link
-        
-//             to="/login"
-//             className={
-//               window.location.pathname === "/login"
-//                 ? "nav-link active"
-//                 : "nav-link"
-//             }
-//           >
-//             Logout
-          
-//         </Link>
-
-//       </Nav>
-//     </Navbar.Collapse>
-//   </Navbar>
-
-// );
-
-
 import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
@@ -168,7 +81,7 @@ export class TopNavbar extends React.Component {
                 <Link
                 to="/user"
                 className={
-                  window.location.pathname === "/user"
+                  window.location.pathname === "/user" || window.location.pathname === "/user"
                   ? "nav-link active"
                 : "nav-link"
               }
@@ -181,7 +94,7 @@ export class TopNavbar extends React.Component {
                 <Link
                 to="/user"
                 className={
-                  window.location.pathname === "/search"
+                  window.location.pathname === "/search" || window.location.pathname === "/search"
                   ? "nav-link active"
                 : "nav-link"
               }
